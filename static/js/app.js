3fc27a498bd5a1f2638396d5c8efc5c67b67c890
smartsingh <smartsingh@gmail.com>
2018-11-30 14:23:55 -0500

    added metadata functionality

--- static/js/app.js
+++ static/js/app.js
@@ -3,22 +3,33 @@ function buildMetadata(sample) {
   // @TODO: Complete the following function that builds the metadata panel
 
   // Use `d3.json` to fetch the metadata for a sample
+  var url =`/metadata/${sample}`;
+
+  d3.json(url).then(function(sample){
+    console.log(sample);
+
     // Use d3 to select the panel with id of `#sample-metadata`
+    var sample_metadata = d3.select("#sample-metadata");
 
     // Use `.html("") to clear any existing metadata
+    sample_metadata.html("");
 
     // Use `Object.entries` to add each key and value pair to the panel
     // Hint: Inside the loop, you will need to use d3 to append new
     // tags for each key-value in the metadata.
+    Object.entries(sample).forEach(function ([key,value]) {
+      var row = sample_metadata.append("p");
+      row.text(`${key}: ${value}`);
+    });
 
-    // BONUS: Build the Gauge Chart
-    // buildGauge(data.WFREQ);
+  });
 }
 
 function buildCharts(sample) {
 
   // @TODO: Use `d3.json` to fetch the sample data for the plots
-
+  //var url = `/samples/${sample}`;
+  //console.log(sample);
     // @TODO: Build a Bubble Chart using the sample data
 
     // @TODO: Build a Pie Chart
